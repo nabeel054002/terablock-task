85e01d0547f34736bf131b35da8691124293ec5b
bruh <nkhan054002@gmail.com>
2023-11-27 12:32:08 +0530

    stuck on typecasting of canister

--- my-canister-project/src/index.ts
+++ my-canister-project/src/index.ts
@@ -1,39 +1,46 @@
-import { Canister,Variant,text, update, Void, nat32, blob, Vec, nat64 } from 'azle';
-import {query} from 'azle'
-const Approval = Variant({
+import { Canister, Variant, text, Principal, update, Void, nat32, blob, Vec, nat64, CandidType } from 'azle';
+import { query } from 'azle';
+
+const Approvalv = Variant({
   from: text,
   to: text,
   value: nat64,
 });
 
-// type app
+interface Approval {
+  from: string;
+  to: string;
+  value: number;
+}
 
 // Define an array to store approvals
-let approvals: Array<any> = [];
+let approvals: Approval[] = [];
 
 export default Canister({
+  // Actor control to restrict access to the person who deployed the canister
+  // (principal = principal-id):
+
   // Adds an approval to the end of the array
   addApproval: update([blob], Void, (approval: any) => {
     approvals.push(approval); // This change will be persisted
   }),
 
   // Removes the very first element of the array and returns it
-  executeApproval: query([], Approval, () => {
+  executeApproval: query([], Approvalv | Void, () => {
     if (approvals.length > 0) {
-      const executedApproval = approvals.shift();
+      const executedApproval = approvals.splice(0, 1)[0];
       return executedApproval;
     } else {
-      return null;
+      return Void;
     }
-}),
-  
+  }),
 
   // Returns the recent n approvals
-  readApprovals: query([nat32], Vec(Approval), (n) => {
+  readApprovals: query([nat32], Approvalv, async (n) => {
     if (n <= approvals.length) {
-      return approvals.slice(-n);
+      return Vec.from(approvals.slice(-n));
     } else {
-      return [];
+      return Vec.empty();
     }
   }),
 });
